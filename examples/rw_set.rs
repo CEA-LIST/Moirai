500952ca435c015cd615bd63901c0db789f58c61
ekkaia <ekkaiasmith@gmail.com>
2025-04-17 17:28:40 +0200

    Improve performances of partial_cmp on dot

--- examples/rw_set.rs
+++ examples/rw_set.rs
@@ -1,22 +1,22 @@
-use po_crdt::crdt::{aw_set::AWSet, test_util::quadruplet_graph};
+use po_crdt::crdt::{rw_set::RWSet, test_util::quadruplet_graph};
 
 fn main() {
-    let (tcsb_a, tcsb_b, tcsb_c, tcsb_d) = quadruplet_graph::<AWSet<u32>>();
+    let (tcsb_a, tcsb_b, tcsb_c, tcsb_d) = quadruplet_graph::<RWSet<u32>>();
 
     let mut tcsb_arr = [tcsb_a, tcsb_b, tcsb_c, tcsb_d];
 
-    let max = 100;
+    let max = 1_000;
 
     for x in 0..max {
         for i in 0..tcsb_arr.len() {
             let op = if x >= max / 2 {
-                AWSet::Remove(max - x)
+                RWSet::Remove(max - x)
             } else {
-                AWSet::Add(x)
+                RWSet::Add(x)
             };
             let event = tcsb_arr[i].tc_bcast(op);
             for j in 0..tcsb_arr.len() {
-                if i != j && i != 0 {
+                if i != j {
                     tcsb_arr[j].try_deliver(event.clone());
                 }
             }
