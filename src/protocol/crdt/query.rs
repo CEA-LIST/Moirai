ecf437c5e09171412ec77b0b3ae3f0c976540167
leo-olivier <leo.olivier@cea.fr>
2025-10-24 12:45:24 +0200

    nested custom queries

--- src/protocol/crdt/query.rs
+++ src/protocol/crdt/query.rs
@@ -5,7 +5,7 @@ pub trait QueryOperation {
 }
 
 #[derive(Debug)]
-pub struct Read<Crdt>(std::marker::PhantomData<Crdt>);
+pub struct Read<V>(std::marker::PhantomData<V>);
 
 impl<V> QueryOperation for Read<V> {
     type Response = V;
@@ -33,6 +33,10 @@ where
     type Response = bool;
 }
 
+/// A query operation to get the value associated with a key.
+/// # Type Parameters
+/// - `K`: The type of the key.
+/// - `V`: The type of the returned value.
 pub struct Get<K, V>(pub K, std::marker::PhantomData<V>);
 
 impl<K, V> Get<K, V> {
@@ -43,4 +47,22 @@ impl<K, V> Get<K, V> {
 
 impl<K, V> QueryOperation for Get<K, V> {
     type Response = Option<V>;
+}
+
+pub struct NestedGet<K, Q> {
+    pub key: K,
+    pub nested_query: Q,
+}
+
+impl<K, Q> NestedGet<K, Q> {
+    pub fn new(key: K, nested_query: Q) -> Self {
+        Self { key, nested_query }
+    }
+}
+
+impl<K, Q> QueryOperation for NestedGet<K, Q>
+where
+    Q: QueryOperation,
+{
+    type Response = Option<Q::Response>;
 }
